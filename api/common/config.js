3a1f17f443c9330d43bc4b3f846de6d2efd149fa
amansawhney <amansawhney@sawhneygames.com>
2017-10-24 22:42:18 -0400

    fixed config.js

---
+++ api/common/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/',
+mongoDbName: 'tca_db',
+secret:'8b8d37254f12372637eaaeac3d36ceeaf',
+support_email: 'support@tca.com',
+admin_username: 'admin',
+check_max_interval: 240,
+check_min_interval: 15,
+encryption_algorithm:'aes-256-ctr',
+encryption_password:'a1e31f3q',
+json_file_path: '/Users/amansawhney/Development/Misc Projects/tca-Alfred/api/common/data_encrypted.json',
+ };
